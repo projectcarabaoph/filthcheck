149e8d32e608b0ffcdc5de82592d1f3716c0c623
josephencila <joseph.mesia.encila@gmail.com>
2025-03-11 22:29:11 +0800

    feat: auth navigation

---
+++ app/auth/_components/auth-navigation.tsx
@@ -0,0 +1,31 @@
+
+'use client';
+
+import Link from 'next/link';
+import paths from '@/utils/paths/paths.config';
+import { AvatarHolderSvg } from '@/public/assets/svgs/index';
+
+const AuthNavigation = () => {
+
+    return (
+        <nav className="bg-white shadow-sm fixed top-0 w-full z-50">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-between h-16">
+                    <div className="flex items-center">
+                        <div className="flex-shrink-0 flex items-center">
+                            <Link href={paths.app.home} className="flex flex-row gap-2 items-center justify-center ml-2 text-xl font-bold text-gray-900">
+                                <AvatarHolderSvg className="h-8 w-8" />
+                                <span>FilthCheck</span>
+                            </Link >
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </nav>
+    )
+}
+
+export default AuthNavigation
--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,8 +1,10 @@
 import SignInForm from "@/app/auth/sign-in/_components/sign-in-form"
+import AuthNavigation from "../_components/auth-navigation"
 
 const SignIn = () => {
     return (
         <div className='flex w-full min-h-dvh h-auto justify-center items-center '>
+            <AuthNavigation />
             <SignInForm />
         </div>
     )
