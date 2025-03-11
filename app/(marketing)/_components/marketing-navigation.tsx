65b4cf5d9b0fb4ffd183a9ca9cd5109f71c0aa2b
josephencila <joseph.mesia.encila@gmail.com>
2025-03-11 12:26:38 +0800

    style and refactor: marketing navigation

--- app/(marketing)/_components/marketing-navigation.tsx
+++ app/(marketing)/_components/marketing-navigation.tsx
@@ -1,7 +1,66 @@
 
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
+import { Menu, X } from 'lucide-react';
+import paths from '@/utils/paths/paths.config';
+
 const MarketingNavigation = () => {
+
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
     return (
-        <div>MarketingNavigation</div>
+        <nav className="bg-white shadow-sm fixed w-full z-50">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-between h-16">
+                    <div className="flex items-center">
+                        <div className="flex-shrink-0 flex items-center">
+                            <Link href={paths.app.home} className="ml-2 text-xl font-bold text-gray-900">FilthCheck</Link >
+                        </div>
+                    </div>
+
+                    <div className="hidden sm:ml-6 sm:flex sm:items-center">
+                        <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+                            <Link href={paths.auth.signIn} className='p-2 rounded-md bg-custome-pink hover:bg-custome-pink/90 text-white
+                             '>Sign In</Link>
+                        </div>
+                    </div>
+
+                    <div className="flex items-center sm:hidden">
+                        <button
+                            onClick={() => setIsOpen(!isOpen)}
+                            className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none"
+                        >
+                            {isOpen ? (
+                                <X className="block h-6 w-6" />
+                            ) : (
+                                <Menu className="block h-6 w-6" />
+                            )}
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            {/* Mobile menu */}
+            {isOpen && (
+                <div className="sm:hidden">
+                    <div className="pt-2 pb-3 space-y-1">
+                        <a href="#" className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
+                            Documentation
+                        </a>
+                    </div>
+                    <div className="pt-4 pb-3 border-t border-gray-200">
+
+                        <div className=" space-y-1  px-2">
+                            <Link href={paths.auth.signIn} className="block w-full text-center p-2 rounded-md border border-custome-pink bg-white text-custome-pink hover:bg-custome-pink hover:text-white">
+                                Sign In
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </nav>
     )
 }
 
--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,8 +1,10 @@
 import type { ReactNode } from "react"
+import MarketingNavigation from "@/app/(marketing)/_components/marketing-navigation"
 
 const MarketingLayout = ({ children }: { children: ReactNode }) => {
     return (
         <main className="min-h-dvh h-auto flex justify-center">
+            <MarketingNavigation />
             {children}
         </main>
     )
