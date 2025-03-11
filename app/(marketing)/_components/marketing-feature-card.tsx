6c743d0f71f4473ac0664354ded2f46fb7d4028f
josephencila <joseph.mesia.encila@gmail.com>
2025-03-11 13:25:20 +0800

    feat: marketing feature card

---
+++ app/(marketing)/_components/marketing-feature-card.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface IMarketingFeatureCard {
+    icon: React.ElementType
+    title: string
+    description: string
+}
+
+export default function MarketingFeatureCard({ icon: Icon, title, description }: IMarketingFeatureCard) {
+    return (
+        <div className="bg-white p-6 rounded-xl shadow-lg hover:shadow-xl transition-shadow">
+            <div className="h-12 w-12 bg-indigo-100 rounded-lg flex items-center justify-center mb-4">
+                <Icon className="h-6 w-6 text-custome-pink" />
+            </div>
+            <h3 className="text-xl font-semibold mb-2">{title}</h3>
+            <p className="text-gray-600">{description}</p>
+        </div>
+    )
+}
+
