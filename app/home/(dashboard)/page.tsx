f67a162ab6104cafe3f330d775a03ab839d9ef11
josephencila <joseph.mesia.encila@gmail.com>
2025-03-13 12:12:13 +0800

    style: dashboard title and description

--- app/home/(dashboard)/page.tsx
+++ app/home/(dashboard)/page.tsx
@@ -3,7 +3,13 @@ import ApiKeyCard from "@/app/home/(dashboard)/_components/api-key-card"
 const Dashboard = () => {
     return (
         <div className="w-full flex flex-col items-center">
-            <div className="flex flex-col items-center p-4 w-full max-w-7xl">
+            <div className="flex flex-col gap-2 p-4 w-full max-w-7xl">
+                <div className="flex flex-col gap-2">
+                    <h1 className="text-lg  font-bold text-gray-900 ">
+                        Dashboard
+                    </h1>
+                    <span className="text-sm">Manage you api information</span>
+                </div>
                 <ApiKeyCard />
             </div>
         </div>
