012680360c4ad9f59e55cb1ef3bb16985cb976be
josephencila <joseph.mesia.encila@gmail.com>
2025-03-13 22:51:36 +0800

    style: dashboard

--- app/home/(dashboard)/page.tsx
+++ app/home/(dashboard)/page.tsx
@@ -1,13 +1,16 @@
-
+import Link from "next/link"
+import { Plus } from 'lucide-react'
 const Dashboard = () => {
     return (
         <div className="w-full flex flex-col items-center">
             <div className="flex flex-col gap-2 p-4 w-full max-w-7xl">
-                <div className="flex flex-col gap-2">
-                    <h1 className="text-lg  font-bold text-gray-900 ">
-                        Dashboard
+                <div className="flex flex-row justify-between gap-2">
+                    <h1 className="text-lg font-bold text-gray-900 ">
+                        Apps
                     </h1>
-                    <span className="text-sm">Manage you api information</span>
+                    <Link href='#project/new' className="bg-custome-pink justify-center text-white px-8 py-2 rounded-lg font-semibold flex items-center gap-2 hover:bg-custome-pink/90 transition-colors">
+                        <Plus className="h-5 w-5" /> Create New App
+                    </Link>
                 </div>
 
             </div>
