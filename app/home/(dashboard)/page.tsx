4ed3c83a4f071a805fb4f9a73d19048a17dcb48f
josephencila <joseph.mesia.encila@gmail.com>
2025-03-14 11:38:01 +0800

    refactor: dashboard, change create app link

--- app/home/(dashboard)/page.tsx
+++ app/home/(dashboard)/page.tsx
@@ -2,8 +2,11 @@ import Link from "next/link"
 import { Plus } from 'lucide-react'
 
 import type { TAppCard } from "@/app/home/(dashboard)/_types"
+import paths from "@/utils/paths/paths.config"
+
 import ListComponent from "@/components/shared/list-component"
 import AppCard from "@/app/home/(dashboard)/_components/app-card"
+
 
 const appData: TAppCard[] = [
     {
@@ -31,7 +34,7 @@ const Dashboard = () => {
                     <h1 className="text-lg font-bold text-gray-900 ">
                         Apps
                     </h1>
-                    <Link href='#project/new' className="bg-custome-pink justify-center text-white px-8 py-2 rounded-lg font-semibold flex items-center gap-2 hover:bg-custome-pink/90 transition-colors">
+                    <Link href={paths.app.newProject} className="bg-custome-pink justify-center text-white px-8 py-2 rounded-lg font-semibold flex items-center gap-2 hover:bg-custome-pink/90 transition-colors">
                         <Plus className="h-5 w-5" /> Create New App
                     </Link>
                 </div>
