30b7122bec91b77abfb80fa3a90fdfc1487e1b01
josephencila <joseph.mesia.encila@gmail.com>
2025-03-14 13:10:56 +0800

    style: create new project form

--- app/home/project/new/create-new-project-form.tsx
+++ app/home/project/new/create-new-project-form.tsx
@@ -3,6 +3,9 @@
 import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { RefreshCw, Globe } from 'lucide-react'
+
+
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 
@@ -41,7 +44,7 @@ export default function CreateNewProjecForm() {
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-full p-4 bg-white rounded-lg outline outline-1 outline-slate-200">
       <div className='flex flex-col gap-1'>
-        <label htmlFor='title' className='text-gray-700 font-medium'>App title</label>
+        <label htmlFor='title' className='text-gray-700 font-medium'>App Title</label>
         <div className="relative">
           <Input
             type='text'
@@ -67,10 +70,20 @@ export default function CreateNewProjecForm() {
       <div className='flex flex-col gap-1'>
         <label htmlFor='title' className='text-gray-700 font-medium'>Plan</label>
 
-        <div className='w-full h-full min-h-48 min-w-48 md:max-w-48 max-h-48 rounded-md outline-[1px] outline outline-custome-pink p-4'>
+        <div className=' flex flex-col gap-2 w-full h-full min-h-56  md:max-w-56 max-h-56 rounded-md outline-[1px] outline outline-custome-pink p-4'>
           <div className='flex flex-row justify-between gap-2 items-center'>
             <small className='bg-custome-pink text-white p-1 rounded-md'>Free</small>
             <small>₱0/mo</small>
+          </div>
+          <div className='flex flex-col gap-2 mt-4'>
+            <div className='flex flex-row gap-2'>
+              <RefreshCw className='w-5 h-5 text-slate-500' />
+              <p>Unlimited post request</p>
+            </div>
+            <div className='flex flex-row gap-2'>
+              <Globe className='w-5 h-5 text-slate-500' />
+              <p>south-east-1</p>
+            </div>
           </div>
         </div>
 
@@ -89,7 +102,7 @@ export default function CreateNewProjecForm() {
       </div>
 
       <div className="mt-4 flex flex-row justify-end ">
-        <Button type='submit' disabled={!isDirty} className="bg-custome-pink disabled:opacity-5 w-full md:max-w-40 justify-center text-white px-8 py-2 rounded-lg font-semibold flex items-center gap-2 hover:bg-custome-pink/90 transition-colors">
+        <Button type='submit' disabled={!isDirty || !isValid || isSubmitting} className="bg-custome-pink disabled:opacity-80 w-full md:max-w-40 justify-center text-white px-8 py-2 rounded-lg font-semibold flex items-center gap-2 hover:bg-custome-pink/90 transition-colors">
           Create App
         </Button>
       </div>
