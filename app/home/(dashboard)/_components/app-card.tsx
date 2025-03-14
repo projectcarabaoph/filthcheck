b17386d4cca419c1a35acb757a8a8cfeeeb58d00
josephencila <joseph.mesia.encila@gmail.com>
2025-03-14 22:00:09 +0800

    refactor: app card

--- app/home/(dashboard)/_components/app-card.tsx
+++ app/home/(dashboard)/_components/app-card.tsx
@@ -1,4 +1,6 @@
 'use client'
+import Link from "next/link";
+import { Settings } from "lucide-react";
 
 import type { TAppCard } from "@/app/home/(dashboard)/_types";
 
@@ -10,16 +12,28 @@ import {
     CardTitle
 } from "@/components/ui/card";
 
-export default function AppCard({ title, description }: TAppCard) {
+export default function AppCard({ data }: { data: TAppCard }) {
+
+    const { title, description, plan, pattern } = data
+
     return (
-        <Card className="w-full glass-card shadow-card overflow-hidden relative transition-all duration-300 hover:bg-slate-100">
-            <CardHeader className="pb-2">
-                <CardTitle className="text-lg font-medium">{title}</CardTitle>
+        <Card className="w-full sm:max-w-md shadow-card overflow-hidden relative transition-all duration-300 hover:bg-slate-50">
+            <div style={{ backgroundImage: pattern }} className="w-full h-full min-h-24 max-h-24">
+            </div>
+            <CardHeader className="pb-2 ">
+                <div className="flex flex-row justify-between items-center gap-2">
+                    <CardTitle className="text-lg font-medium">{title}</CardTitle>
+                    <Link href={'#'}>
+                        <Settings className="w-4 h-4 text-slate-500 hover:text-slate-700" />
+                    </Link>
+                </div>
                 <CardDescription>{description}</CardDescription>
             </CardHeader>
             <CardContent>
                 <div className="relative">
-
+                    <div className="flex flex-col">
+                        <p className="capitalize w-12 bg-indigo-100 text-custome-pink text-center p-1 rounded-md">{plan}</p>
+                    </div>
                 </div>
             </CardContent>
         </Card>
