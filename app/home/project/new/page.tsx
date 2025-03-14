79c5b05ca8ef6b0566d8a379579fcfbf7a082df2
josephencila <joseph.mesia.encila@gmail.com>
2025-03-14 13:14:06 +0800

    style: new project page

--- app/home/project/new/page.tsx
+++ app/home/project/new/page.tsx
@@ -2,16 +2,16 @@ import CreateNewProjecForm from "@/app/home/project/new/create-new-project-form"
 
 const NewProject = () => {
     return (
-        <div className=" flex flex-col gap-2 ">
+        <div className=" flex flex-col items-center  gap-2 ">
             <div className="max-w-7xl w-full p-2 flex flex-col">
                 <div className="flex flex-col gap-2 p-2">
                     <h1 className="text-lg font-bold text-gray-900 ">
                         Create New App
                     </h1>
                 </div>
-            </div>
-            <div className="flex flex-col gap-2 p-2">
-                <CreateNewProjecForm />
+                <div className="flex flex-col gap-2 p-2">
+                    <CreateNewProjecForm />
+                </div>
             </div>
         </div>
     )
