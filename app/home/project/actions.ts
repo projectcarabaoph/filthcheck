9e34875b5c9dd18e646d8c0317d66a4c02240af4
josephencila <joseph.mesia.encila@gmail.com>
2025-03-14 22:37:20 +0800

    refactor: create new project action, implement generate random pattern

--- app/home/project/actions.ts
+++ app/home/project/actions.ts
@@ -4,16 +4,19 @@ import { revalidatePath } from "next/cache"
 import { serverClient } from "@/utils/supabase/server-client"
 import type { TNewProjectSchema } from "./new/create-new-project-form"
 import paths from "@/utils/paths/paths.config"
+import { generateRandomPattern } from "@/utils/misc/generate-random-pattern"
 
 export const createNewProject = async (formData: TNewProjectSchema) => {
 
     const supabase = await serverClient()
+    const pattern = generateRandomPattern()
 
     const { data, error } = await supabase
         .from('projects')
         .insert({
             title: formData.title.trim(),
-            description: formData.description.trim()
+            description: formData.description.trim(),
+            pattern: pattern
         })
 
     if (error) throw new Error(error.message)
