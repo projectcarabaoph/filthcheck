7f2065a94a694e618895c1ed22a2e4ab7c643688
josephencila <joseph.mesia.encila@gmail.com>
2025-03-19 22:02:56 +0800

    refactor: dashboard page create app link, added disable condition

--- app/home/(dashboard)/page.tsx
+++ app/home/(dashboard)/page.tsx
@@ -28,7 +28,9 @@ const Dashboard = async () => {
                     <h1 className="text-lg font-bold text-gray-900 ">
                         Apps
                     </h1>
-                    <Link href={paths.app.newProject} className="bg-custome-pink justify-center text-white px-4 py-2 rounded-md flex items-center gap-2 hover:bg-custome-pink/90 transition-colors">
+                    <Link as={projectData?.length ? 'div' : 'a'}
+                        href={paths.app.newProject}
+                        className={`${projectData?.length ? 'bg-gray-300 ' : 'bg-custome-pink hover:bg-custome-pink/90 '} justify-center text-white px-4 py-2 rounded-md flex items-center gap-2 transition-colors`}>
                         <Plus className="h-5 w-5" />
                         <span>Create New App</span>
                     </Link>
