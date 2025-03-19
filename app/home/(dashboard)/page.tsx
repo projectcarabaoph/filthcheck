02e702a0e9f17e4468e3f09c9d1e7e46a109e48f
josephencila <joseph.mesia.encila@gmail.com>
2025-03-19 22:44:07 +0800

    chore: remove console.log

--- app/home/(dashboard)/page.tsx
+++ app/home/(dashboard)/page.tsx
@@ -23,8 +23,6 @@ const Dashboard = async () => {
 
     const isProjectData = projectData && projectData.length > 0
 
-    console.log(isProjectData)
-
     return (
         <div className="w-full flex flex-col items-center">
             <div className="flex flex-col gap-2 p-4 w-full max-w-7xl">
