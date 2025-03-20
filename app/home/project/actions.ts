67fe258623eae43cd1a9be3632b4e04ed9a9cbf5
josephencila <joseph.mesia.encila@gmail.com>
2025-03-20 22:25:56 +0800

    refactor: create new project action, implement generate api token

--- app/home/project/actions.ts
+++ app/home/project/actions.ts
@@ -1,27 +1,49 @@
 'use server'
 
 import { revalidatePath } from "next/cache"
+
 import { serverClient } from "@/utils/supabase/server-client"
-import type { TNewProjectSchema } from "./new/create-new-project-form"
 import paths from "@/utils/paths/paths.config"
 import { generateRandomPattern } from "@/utils/misc/generate-random-pattern"
+import { generateApiToken } from "@/utils/misc/generate-api-token"
+
+import type { TNewProjectSchema } from "@/app/home/project/new/create-new-project-form"
 
 export const createNewProject = async (formData: TNewProjectSchema) => {
 
     const supabase = await serverClient()
     const pattern = generateRandomPattern()
 
-    const { data, error } = await supabase
+    const { data: { user } } = await supabase.auth.getUser()
+
+    const generatedApiToken = generateApiToken({
+        id: user?.id as string,
+        email: user?.email as string
+    })
+
+    if (!generatedApiToken) throw new Error('Failed to generate API token')
+
+    const { data: projectData, error: projectError } = await supabase
         .from('projects')
         .insert({
             title: formData.title.trim(),
             description: formData.description.trim(),
             pattern: pattern
+        }).select('*').single()
+
+    if (projectError) throw new Error(projectError.message)
+
+    const { data: apiData, error: apiError } = await supabase
+        .from('api_keys')
+        .insert({
+            project_id: projectData?.project_id,
+            api_key: generatedApiToken
         })
 
-    if (error) throw new Error(error.message)
+    if (apiError) throw new Error(apiError.message)
+
 
     revalidatePath(paths.app.home)
-    return data
+    return apiData
 
 }
