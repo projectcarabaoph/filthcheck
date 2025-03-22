2a97b13d448b93454ed43fe3e4f486616008e1b7
josephencila <joseph.mesia.encila@gmail.com>
2025-03-22 12:56:04 +0800

    refactor: middleware client

--- utils/supabase/server-client.ts
+++ utils/supabase/server-client.ts
@@ -69,14 +69,13 @@ export async function middlewareClient(request: NextRequest) {
 
     const { pathname } = request.nextUrl
     const requestUrl = request.nextUrl.clone()
-    const privatePaths = Object.values(paths.app).includes(pathname)
 
-    if (user && !privatePaths) {
+    if (user && !pathname.startsWith(paths.app.home)) {
         requestUrl.pathname = paths.app.home
         return NextResponse.redirect(requestUrl)
     }
 
-    if (!user && privatePaths) {
+    if (!user && pathname.startsWith(paths.app.home)) {
         requestUrl.pathname = paths.auth.signIn
         return NextResponse.redirect(requestUrl)
     }
