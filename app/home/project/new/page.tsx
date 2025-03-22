e2519cbfa7b70a13973ce71cae4de2d5d1614c10
josephencila <joseph.mesia.encila@gmail.com>
2025-03-22 11:58:09 +0800

    chore: accept conflict

--- app/home/project/new/page.tsx
+++ app/home/project/new/page.tsx
@@ -2,7 +2,7 @@ import CreateNewProjecForm from "@/app/home/project/new/create-new-project-form"
 
 const NewProject = () => {
     return (
-        <div className=" flex flex-col items-center  gap-2 ">
+        <div className=" flex justify-center ">
             <div className="max-w-7xl w-full p-2 flex flex-col">
                 <div className="flex flex-col gap-2 p-2">
                     <h1 className="text-lg font-bold text-gray-900 ">
@@ -12,6 +12,9 @@ const NewProject = () => {
                 <div className="flex flex-col gap-2 p-2">
                     <CreateNewProjecForm />
                 </div>
+            </div>
+            <div className="flex flex-col gap-2 p-2">
+                <CreateNewProjecForm />
             </div>
         </div>
     )
