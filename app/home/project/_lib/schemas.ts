edb0baa1a9751e619bd3543d3c0cb09ad22fec75
josephencila <joseph.mesia.encila@gmail.com>
2025-03-25 22:48:33 +0800

    feat: allowed domain schema

--- app/home/project/_lib/schemas.ts
+++ app/home/project/_lib/schemas.ts
@@ -6,3 +6,13 @@ export const testApiCardSchema = z.object({
         .url({ message: "Invalid image URL." })
 })
 
+const domainSchema = z.object({
+    domain: z.union([
+        z.string().url({ message: "Invalid domain URL." }),
+        z.literal("")
+    ]),
+});
+
+export const allowedDomainsSchema = z.object({
+    domains: z.array(domainSchema)
+});
