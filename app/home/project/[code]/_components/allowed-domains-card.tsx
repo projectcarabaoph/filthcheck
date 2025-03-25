85776589262d0bc27413e08e6a3bdf4308fd95f3
josephencila <joseph.mesia.encila@gmail.com>
2025-03-25 22:17:56 +0800

    refactor: allowed domains card, added inputs for domains

--- app/home/project/[code]/_components/allowed-domains-card.tsx
+++ app/home/project/[code]/_components/allowed-domains-card.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
 import {
     Card,
     CardContent,
@@ -15,19 +16,35 @@ interface IAllowedDomainsCard {
 
 export default function AllowedDomainsCard({ domains }: IAllowedDomainsCard) {
 
+    const maxDomainLength: number = 2
 
     return (
         <Card className="w-full glass-card shadow-card overflow-hidden relative transition-all duration-300 hover:shadow-elevated">
             <CardHeader className="pb-2">
                 <CardTitle className="text-lg font-medium">Allowed Domains</CardTitle>
-                <CardDescription>   Whitelisted domains that are authorized to make requests using this API key.</CardDescription>
+                <CardDescription>Whitelisted domains that are authorized to make requests using this API key.</CardDescription>
             </CardHeader>
             <CardContent>
-                <div >
-                    <Input
-                        className="pr-12 font-mono text-sm bg-secondary/50"
-                    />
-
+                <div className="flex flex-col gap-4">
+                    {Array(maxDomainLength)
+                        .fill(null)
+                        .map((_, i) => (
+                            <Input
+                                key={i}
+                                value={domains?.[i] ?? ""}
+                                placeholder="www.example-domain.com"
+                                className="pr-12 font-mono text-sm bg-secondary/50"
+                            />
+                        ))}
+                    <div className="flex justify-end">
+                        <Button
+                            variant='default'
+                            type="submit"
+                            className="bg-custome-pink hover:bg-custome-pink/90"
+                        >
+                            Save
+                        </Button>
+                    </div>
                 </div>
             </CardContent>
         </Card>
