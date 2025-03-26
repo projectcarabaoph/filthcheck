7feecabd5b4f3a49b3002baf01067e664b7c3a29
josephencila <joseph.mesia.encila@gmail.com>
2025-03-26 21:47:16 +0800

    fix: auth callback not working, change project_code to code

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -9,11 +9,11 @@ export async function GET(request: NextRequest) {
     const keys = clientKeys()
     const requestUrl = new URL(request.url)
 
-    const project_code = requestUrl.searchParams.get("project_code")
+    const code = requestUrl.searchParams.get("code")
 
-    if (project_code) {
+    if (code) {
         const supabase = await serverClient()
-        await supabase.auth.exchangeCodeForSession(project_code)
+        await supabase.auth.exchangeCodeForSession(code)
     }
     return NextResponse.redirect(
         new URL(paths.app.home, keys.devBaseURL)
