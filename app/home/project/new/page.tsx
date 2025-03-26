e1ff42dacccffb1e9e3ee9193574df9307860c59
josephencila <joseph.mesia.encila@gmail.com>
2025-03-26 11:16:22 +0800

    fix: merge conflict

--- app/home/project/new/page.tsx
+++ app/home/project/new/page.tsx
@@ -1,81 +1,20 @@
-
 import CreateNewProjecForm from "@/app/home/project/new/_components/create-new-project-form"
 
 const NewProject = () => {
     return (
         <div className=" flex flex-col items-center  gap-2 ">
-            import CreateNewProjecForm from "@/app/home/project/new/create-new-project-form"
-
-const NewProject = () => {
-    return (
-            <div className=" flex justify-center ">
-                <div className="max-w-7xl w-full p-2 flex flex-col">
-                    <div className="flex flex-col gap-2 p-2">
-                        <h1 className="text-lg font-bold text-gray-900 ">
-                            Create New App
-                        </h1>
-                    </div>
-                    <div className="flex flex-col gap-2 p-2">
-                        <CreateNewProjecForm />
-                    </div>
+            <div className="max-w-7xl w-full p-2 flex flex-col">
+                <div className="flex flex-col gap-2 p-2">
+                    <h1 className="text-lg font-bold text-gray-900 ">
+                        Create New App
+                    </h1>
                 </div>
                 <div className="flex flex-col gap-2 p-2">
                     <CreateNewProjecForm />
                 </div>
             </div>
-            )
-            import CreateNewProjecForm from "@/app/home/project/new/create-new-project-form"
-
-const NewProject = () => {
-    return (
-            <div className=" flex justify-center ">
-                <div className="max-w-7xl w-full p-2 flex flex-col">
-                    <div className="flex flex-col gap-2 p-2">
-                        <h1 className="text-lg font-bold text-gray-900 ">
-                            Create New App
-                        </h1>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-2 p-2">
-                    <CreateNewProjecForm />
-                </div>
-            </div>
-            )
-            import CreateNewProjecForm from "@/app/home/project/new/create-new-project-form"
-
-const NewProject = () => {
-    return (
-            <div className=" flex justify-center ">
-                <div className="max-w-7xl w-full p-2 flex flex-col">
-                    <div className="flex flex-col gap-2 p-2">
-                        <h1 className="text-lg font-bold text-gray-900 ">
-                            Create New App
-                        </h1>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-2 p-2">
-                    <CreateNewProjecForm />
-                </div>
-            </div>
-            )
-            import CreateNewProjecForm from "@/app/home/project/new/create-new-project-form"
-
-const NewProject = () => {
-    return (
-            <div className=" flex justify-center ">
-                <div className="max-w-7xl w-full p-2 flex flex-col">
-                    <div className="flex flex-col gap-2 p-2">
-                        <h1 className="text-lg font-bold text-gray-900 ">
-                            Create New App
-                        </h1>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-2 p-2">
-                    <CreateNewProjecForm />
-                </div>
-            </div>
-            )
-
+        </div>
+    )
 }
 
-            export default NewProject
+export default NewProject
