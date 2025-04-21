d1b041a31a11d62115564d8d5ccc670cd1735591
josephencila <joseph.mesia.encila@gmail.com>
2025-04-21 10:42:02 +0800

    refactor: marketing feature card, types and icon, replace with iconify

--- app/(marketing)/_components/marketing-feature-card.tsx
+++ app/(marketing)/_components/marketing-feature-card.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import { Icon } from '@iconify/react'
 
 interface IMarketingFeatureCard {
-    icon: React.ElementType
+    icon: string
     title: string
     description: string
 }
 
-export default function MarketingFeatureCard({ icon: Icon, title, description }: IMarketingFeatureCard) {
+export default function MarketingFeatureCard({ icon, title, description }: IMarketingFeatureCard) {
     return (
         <div className="bg-white p-6 rounded-md outline outline-[1px] outline-gray-200 hover:outline-[2px]">
             <div className="h-12 w-12 bg-indigo-100 rounded-lg flex items-center justify-center mb-4">
-                <Icon className="h-6 w-6 text-custome-pink" />
+                <Icon icon={icon} className="h-6 w-6 text-custome-pink" />
             </div>
             <h3 className="text-xl font-semibold mb-2">{title}</h3>
             <p className="text-gray-600">{description}</p>
