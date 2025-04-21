390f3e43c17744680b8805262ba4a52e3be0490c
josephencila <joseph.mesia.encila@gmail.com>
2025-04-21 11:37:34 +0800

    style: marketing hero

--- app/(marketing)/_components/marketing-hero.tsx
+++ app/(marketing)/_components/marketing-hero.tsx
@@ -4,17 +4,18 @@ import paths from "@/utils/paths/paths.config"
 
 const MarketingHero = () => {
     return (
-        <div className="relative overflow-hidden text-center py-20">
-            <h1 className="text-5xl md:text-6xl font-bold text-gray-900 mb-6 ">
-                A simple API for detecting
-                <span className="text-custome-pink"> NSFW </span>
-                image content
-            </h1>
-            <p className="text-xl text-gray-600 mb-8 max-w-3xl mx-auto">
-
-                It utilizes onnx models to analyze images and classify them as either Safe or Not Safe For Work (NSFW)
+        <div className="relative overflow-hidden flex flex-col items-center  text-center py-20">
+            <div className="flex flex-col justify-center items-center w-full max-w-4xl">
+                <h1 className="text-4xl md:text-6xl font-bold text-gray-900 mb-6 leading-tight">
+                    A simple API for detecting
+                    <span className="text-custome-pink"> NSFW </span>
+                    Nudity image content
+                </h1>
+            </div>
+            <p className="text-lg text-gray-600 mb-8 max-w-2xl mx-auto">
+                It utilizes ONNX models to analyze images for nudity and classifies them as either Safe or Not Safe For Work (NSFW).
             </p>
-            <div className="flex gap-4 justify-center flex-col md:flex-row">
+            <div className="w-full flex gap-4 justify-center flex-col md:flex-row">
                 <Link href={paths.auth.signIn} className="bg-custome-pink justify-center text-white px-8 py-2 rounded-lg font-semibold flex items-center gap-2 hover:bg-custome-pink/90 transition-colors">
                     Get Started <ArrowRight className="h-5 w-5" />
                 </Link>
