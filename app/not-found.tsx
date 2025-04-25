07e8f7caf63fbf857e0ba55abfb2016829c6ec48
josephencila <joseph.mesia.encila@gmail.com>
2025-04-25 11:56:16 +0800

    refactor: not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,8 +5,9 @@ const NotFound = () => {
     return (
         <div className='flex w-full min-h-dvh h-auto justify-center items-center '>
             <div className="flex flex-col items-center justify-center gap-2">
-                <h1>404 Page not found</h1>
-                <Link href={paths.app.home} className='bg-slate-400 hover:bg-slate-400/90 p-2 rounded-md text-white w-24 text-center'>Go back</Link>
+                <h1 className="text-md md:text-lg lg:text-xl font-bold">404 Page not found</h1>
+                <span>An error occured, we cannot find the page you we&apos;re looking for.</span>
+                <Link href={paths.app.home} className='bg-custome-pink hover:bg-custome-pink/90 p-2 rounded-md text-white w-24 text-center'>Go back</Link>
             </div>
         </div>
     )
