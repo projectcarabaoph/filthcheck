1f3cd92455bc5a2c52fcef4d68c5fc7285ed3f9e
josephencila <joseph.mesia.encila@gmail.com>
2025-04-26 12:08:07 +0800

    style: marketing cta, center h2

--- app/(marketing)/_components/marketing-cta.tsx
+++ app/(marketing)/_components/marketing-cta.tsx
@@ -6,7 +6,7 @@ export default function MarketingCta() {
     return (
         <div className="flex flex-col">
             <div className="flex flex-col px-4 py-16 items-center">
-                <h2 className="text-3xl font-bold text-gray-900 mb-4">Ready to get started?</h2>
+                <h2 className="text-3xl text-center font-bold text-gray-900 mb-4">Ready to get started?</h2>
                 <p className="text-xl text-gray-600 mb-8 text-center">
                     Join Tens of users building amazing applications with our API platform.
                 </p>
