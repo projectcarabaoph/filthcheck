96b58e53af5c4da20b47f55077454509487111d7
josephencila <joseph.mesia.encila@gmail.com>
2025-04-26 12:12:15 +0800

    refactor: marketing hero, added condition to check if user already signed in

--- app/(marketing)/_components/marketing-hero.tsx
+++ app/(marketing)/_components/marketing-hero.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link"
 import { ArrowRight } from "lucide-react"
 import paths from "@/utils/paths/paths.config"
+import { serverClient } from "@/utils/supabase/server-client"
 
-const MarketingHero = () => {
+const MarketingHero = async () => {
+    const supabase = await serverClient()
+    const { data: { user } } = await supabase.auth.getUser()
     return (
         <div className="relative overflow-hidden flex flex-col items-center  text-center py-20">
             <div className="flex flex-col justify-center items-center w-full max-w-4xl">
@@ -16,7 +19,7 @@ const MarketingHero = () => {
                 It utilizes ONNX models to analyze images for nudity and classifies them as either Safe or Not Safe For Work (NSFW).
             </p>
             <div className="w-full flex gap-4 justify-center flex-col md:flex-row">
-                <Link href={paths.auth.signIn} className="bg-custome-pink justify-center text-white px-8 py-2 rounded-lg font-semibold flex items-center gap-2 hover:bg-custome-pink/90 transition-colors">
+                <Link href={user ? paths.app.home : paths.auth.signIn} className="bg-custome-pink justify-center text-white px-8 py-2 rounded-lg font-semibold flex items-center gap-2 hover:bg-custome-pink/90 transition-colors">
                     Get Started <ArrowRight className="h-5 w-5" />
                 </Link>
                 <Link href="#" className="border-2 border-gray-300 text-gray-700 px-8 py-2 rounded-lg font-semibold hover:border-gray-400 transition-colors">
