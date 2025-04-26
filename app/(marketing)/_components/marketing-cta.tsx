76b111eb8a62df0776c00528f34108e07b329047
josephencila <joseph.mesia.encila@gmail.com>
2025-04-26 12:10:24 +0800

    refactor: marketing cta, added condition to check if user already signed in

--- app/(marketing)/_components/marketing-cta.tsx
+++ app/(marketing)/_components/marketing-cta.tsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
 import paths from "@/utils/paths/paths.config";
+import { serverClient } from "@/utils/supabase/server-client";
 
-export default function MarketingCta() {
+export default async function MarketingCta() {
+    const supabase = await serverClient()
+    const { data: { user } } = await supabase.auth.getUser()
+
     return (
         <div className="flex flex-col">
             <div className="flex flex-col px-4 py-16 items-center">
@@ -10,7 +14,7 @@ export default function MarketingCta() {
                 <p className="text-xl text-gray-600 mb-8 text-center">
                     Join Tens of users building amazing applications with our API platform.
                 </p>
-                <Link href={paths.auth.signIn} className="bg-custome-pink text-white  px-8 py-2 rounded-md font-semibold flex items-center gap-2 hover:bg-custome-pink/90 transition-colors ">
+                <Link href={user ? paths.app.home : paths.auth.signIn} className="bg-custome-pink text-white  px-8 py-2 rounded-md font-semibold flex items-center gap-2 hover:bg-custome-pink/90 transition-colors ">
                     Start Building Today <ArrowRight className="h-5 w-5" />
                 </Link>
             </div>
