a26e5aa8dd8ad5658e183b098c745d39b49e7512
josephencila <joseph.mesia.encila@gmail.com>
2025-04-26 11:12:31 +0800

    refactor: create new project schema, reduce title to 30 char and description to 50 char

--- app/home/project/_lib/schemas.ts
+++ app/home/project/_lib/schemas.ts
@@ -23,10 +23,10 @@ export const newProjectSchema = z.object({
     title: z.
         string()
         .min(1, { message: "Title is required." })
-        .max(50, { message: 'Title cannot exceed 50 characters.' }),
+        .max(30, { message: 'Title cannot exceed 30 characters.' }),
     description: z
         .string()
         .min(1, { message: "Description is required." })
-        .max(200, { message: 'Description cannot exceed 200 characters.' }),
+        .max(50, { message: 'Description cannot exceed 50 characters.' }),
 })
 
