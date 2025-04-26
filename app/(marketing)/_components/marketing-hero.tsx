0d3e751766a0b4a7e670e9f9eebb74e3bf3e1a61
josephencila <joseph.mesia.encila@gmail.com>
2025-04-26 12:18:36 +0800

    refactor: marketing hero, added path to documetation link

--- app/(marketing)/_components/marketing-hero.tsx
+++ app/(marketing)/_components/marketing-hero.tsx
@@ -24,7 +24,7 @@ export default async function MarketingHero() {
                 <Link href={user ? paths.app.home : paths.auth.signIn} className="bg-custome-pink justify-center text-white px-8 py-2 rounded-lg font-semibold flex items-center gap-2 hover:bg-custome-pink/90 transition-colors">
                     Get Started <ArrowRight className="h-5 w-5" />
                 </Link>
-                <Link href="#" className="border-2 border-gray-300 text-gray-700 px-8 py-2 rounded-lg font-semibold hover:border-gray-400 transition-colors">
+                <Link href={paths.documentation.docs} className="border-2 border-gray-300 text-gray-700 px-8 py-2 rounded-lg font-semibold hover:border-gray-400 transition-colors">
                     View Documentation
                 </Link>
             </div>
