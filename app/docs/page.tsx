809f09a46a921fc0242fb3a3ddbc9aada49fab7b
josephencila <joseph.mesia.encila@gmail.com>
2025-04-28 19:35:41 +0800

    refactor: docs page, switch path of go back link if user is signed in

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -5,11 +5,14 @@ import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import Link from 'next/link';
 import paths from '@/utils/paths/paths.config';
+import { useUser } from '@/context/user-context';
 
 const Docs = () => {
 
     const filePath = path.join(process.cwd(), 'app/docs/_lib/markdown.txt');
     const markdownText = fs.readFileSync(filePath, 'utf-8');
+
+    const user = useUser()
 
     return (
         <div className="bg-white max-w-4xl h-auto flex flex-col justify-center items-center rounded-md p-4 mx-auto">
@@ -22,7 +25,7 @@ const Docs = () => {
                 </ReactMarkdown>
             </div>
             <div className='flex flex-row justify-between'>
-                <Link className='text-md hover:underline' href={paths.marketing.landing}>
+                <Link className='text-md hover:underline' href={user.id ? paths.app.home : paths.marketing.landing}>
                     Go back
                 </Link>
             </div>
