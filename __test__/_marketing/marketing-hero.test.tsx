b5497146ce9667ec5d835d9de23cc10e15f2a0ca
projectcarabaoph <projectcarabao@gmail.com>
2025-06-16 13:20:23 +0800

    chore

--- __test__/_marketing/marketing-hero.test.tsx
+++ __test__/_marketing/marketing-hero.test.tsx
@@ -10,7 +10,6 @@ jest.mock("@/utils/supabase/server-client", () => ({
     serverClient: jest.fn(),
 }))
 
-
 describe("MarketingHero SSR", () => {
     it("renders 'Get Started' link to sign-in page when user is not logged in", async () => {
         ; (serverClient as jest.Mock).mockResolvedValue({
